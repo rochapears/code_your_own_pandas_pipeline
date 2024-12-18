969c249ec86a36b0feca993e416625a60a5d69a8
Susan Mew <160909835+rochapears@users.noreply.github.com>
2024-12-18 11:43:15 +0000

    Adds functionality to import data into pipeline

--- code_your_own_pandas_pipeline/data_in.py
+++ code_your_own_pandas_pipeline/data_in.py
@@ -6,6 +6,7 @@ folder.
 import pandas as pd
 from loguru import logger
 
+from code_your_own_pandas_pipeline.config import RAW_DATA_DIR
 
 def read_mapping_data() -> pd.DataFrame:
     """
@@ -16,9 +17,15 @@ def read_mapping_data() -> pd.DataFrame:
     pd.DataFrame
         The mapping data.
     """
-    logger.info(f"Reading mapping data from {""}")
+    file_path = RAW_DATA_DIR / "Mapping.csv"
 
-    logger.warning("This function is not yet implemented.")
+    logger.info(f"Reading mapping data from {file_path}")
+
+    mapping_df = pd.read_csv(file_path)
+    
+    return mapping_df
+
+
 
 
 def read_practice_crosstab_data() -> pd.DataFrame:
@@ -30,11 +37,28 @@ def read_practice_crosstab_data() -> pd.DataFrame:
     pd.DataFrame
         The practice crosstab data.
     """
-    logger.info(f"Reading practice crosstab data from {""}")
 
-    logger.warning("This function is not yet implemented.")
+    file_paths = [
+        "Practice_Level_Crosstab_Jul_24.csv",
+        "Practice_Level_Crosstab_Aug_24.csv",
+        "Practice_Level_Crosstab_Sep_24.csv",  
+    ]
+
+    practice_datasets = []
+    for file_path in file_paths:
+        full_file_path = RAW_DATA_DIR / file_path
+        logger.info(f"Reading practice crosstab data from {full_file_path}")
+
+        data = pd.read_csv(full_file_path)
+        practice_datasets.append(data)
+
+    logger.info("Concatenating the practice crosstab datasets into one dataset")
+    read_practice_crosstab_data = pd.concat(practice_datasets)
+
+    return read_practice_crosstab_data
 
 
-if __name__ == "__main__":
-    read_mapping_data().head()
-    read_practice_crosstab_data().head()
+    
+
+
+   
